4baec492b01e58bd37b86f15e21b55149eacdd0f
Kemal Ergorun <kemalergorun@gmail.com>
2024-09-30 21:40:22 +0300

    30/09/24

--- src/main/java/yeni2/Practice14.java
+++ src/main/java/yeni2/Practice14.java
@@ -1,7 +1,22 @@
 package yeni2;
 
+import java.util.Scanner;
+
 public class Practice14 {
     public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("sayı gir");
+        int a= input.nextInt();
+
+        int sum=0;
+
+        while (a>0){
+            sum+=a%10;
+            a=a/10;
+
+
+        }
+        System.out.println("sum="+sum);
 
 
 
--- src/main/java/yeni2/Practice15.java
+++ src/main/java/yeni2/Practice15.java
@@ -1,7 +1,29 @@
 package yeni2;
 
+import java.util.Scanner;
+
 public class Practice15 {
     public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println(" give number between 1-10");
+        int number= input.nextInt();
+        int result=1;
+        int i=1;
+
+        while (i<11){
+            if (number<0 || number>10 ){
+                System.out.println("give right number");
+                break;
+            }
+            result=i*number;
+
+            System.out.println(number+"x"+i+"="+(result));
+            i++;
+
+
+
+        }
+
 
 
 
