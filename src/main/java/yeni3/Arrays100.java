b53e8bf07ee913604eacf579375d959377ad575b
Kemal Ergorun <kemalergorun@gmail.com>
2024-10-01 21:59:28 +0300

    01/10/24

--- src/main/java/yeni3/Arrays100.java
+++ src/main/java/yeni3/Arrays100.java
@@ -3,5 +3,9 @@ package yeni3;
 public class Arrays100 {
     public static void main(String[] args) {
 
+
+
+
+
     }
 }
---
+++ src/main/java/yeni3/Arrays102.java
@@ -0,0 +1,29 @@
+package yeni3;
+
+import java.util.Arrays;
+
+public class Arrays102 {
+    public static void main(String[] args) {
+        String[] stdNames=new String[3];
+        String teacherNames[]=new String[6];
+
+        System.out.println(Arrays.toString(stdNames));
+        int numOfStds[]=new int[6];
+        System.out.println(Arrays.toString(numOfStds));
+
+        String b298[]=new String[4];
+        System.out.println(Arrays.toString(b298));
+        b298[0]="kemal";
+        b298[1]="ceylin";
+        b298[2]="betül";
+        b298[3]="baba";
+        System.out.println(Arrays.toString(b298));
+
+
+
+
+
+
+
+    }
+}
