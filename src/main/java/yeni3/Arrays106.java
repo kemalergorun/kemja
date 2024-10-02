8f9af3bb5b2e2f6b85ab22f434891598513ccf76
Kemal Ergorun <kemalergorun@gmail.com>
2024-10-02 21:56:17 +0300

    021024

---
+++ src/main/java/yeni3/Arrays106.java
@@ -0,0 +1,42 @@
+package yeni3;
+
+import java.util.Arrays;
+
+public class Arrays106 {
+    public static void main(String[] args) {
+        int ages[]={12,6,9,18,22,15};
+        System.out.println(Arrays.toString(ages));
+
+        Arrays.sort(ages);
+        System.out.println("after sorting:" +Arrays.toString(ages));
+        // get max and min age
+        // we can use index 0 minumum after sorting
+        //we can last index maximum value length-1
+        int minage=ages[0];
+        int maxage=ages[ages.length-1];
+
+        System.out.println(minage);
+        System.out.println(maxage);
+        //--------------------------
+        int grades[]={59,67,99,20,70,85};
+        System.out.println(Arrays.toString(grades));
+
+        int minGrade=grades[0];
+        int maxGrade=grades[0];
+
+        for (int w : grades){
+            minGrade=Math.min(w, minGrade);
+            maxGrade=Math.max(w,maxGrade);
+
+
+        }
+        System.out.println(minGrade);
+        System.out.println(maxGrade);
+
+
+
+
+
+
+    }
+}
---
+++ src/main/java/yeni3/Arrays107.java
@@ -0,0 +1,22 @@
+package yeni3;
+
+import java.util.Arrays;
+
+public class Arrays107 {
+    public static void main(String[] args) {
+        //two arrays equal
+        int[] a={3,6,8};
+        int[] b={3,8,6};
+        boolean check=a.equals(b);
+        System.out.println(check);
+
+
+
+
+
+
+
+
+
+    }
+}
