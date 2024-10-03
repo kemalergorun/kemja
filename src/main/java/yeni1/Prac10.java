918abcd230de97c05f56efafa4d400d1971282d7
Kemal Ergorun <kemalergorun@gmail.com>
2024-10-03 15:13:00 +0300

    031024j

--- src/main/java/yeni1/Prac10.java
+++ src/main/java/yeni1/Prac10.java
@@ -2,6 +2,23 @@ package yeni1;
 
 public class Prac10 {
     public static void main(String[] args) {
+        String s="learn java earn 1234 money";
+        //money i dollar yap
+        //replace 2. parametredekini birinci yerine koyar
+        String s1=s.replace("money","dollar");
+        System.out.println(s1);
+        //---------------------------------------------
+        //s strigdeki e leri sil
+        String s2=s.replace("e","");
+        System.out.println(s2);
+        //--------------------------------------------------
+        //regular expression düzenli ifade regex
+        String k="Kemal ERgorun";
+
+        // s string de tum rakamları * yap
+        String s3=s.replaceAll("[0-9]","*");
+        System.out.println(s3);
+
 
     }
 }
--- src/main/java/yeni1/Prac9.java
+++ src/main/java/yeni1/Prac9.java
@@ -2,6 +2,35 @@ package yeni1;
 
 public class Prac9 {
     public static void main(String[] args) {
+        //substring index aralığı sub döndürür
+        //substring(0,4)= 0 dahil 4 hariç
+        String s="java is easy";
+        String sub1=s.substring(0,4);
+        System.out.println(sub1);
+        //---------------
+        String sub2=s.substring(5,7);
+        System.out.println(sub2);
+        //------------------------------
+        //tek parametre başlar sona kadar
+        String sub3=s.substring(8);
+        System.out.println(sub3);
+        //---------------------------------
+        //contains metodu true fals döner stringde karakter kontrol eder
+
+        boolean isexist=s.contains("easy");
+        System.out.println(isexist);
+
+        //---------------------------------------
+        //startsWith string ilk karakteri boolean döner
+        boolean isstart=s.startsWith("java");
+        System.out.println(isstart);
+        //-----------------------------------------
+        //endswith son karakter kontrol boolean döner
+        boolean isend=s.endsWith("easy");
+        System.out.println(isend);
+        //-------------------------------------------
+
+
 
     }
 }
