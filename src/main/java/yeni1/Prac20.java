56fb172f25abb6b8424b4510ce8a01426de74077
Kemal Ergorun <kemalergorun@gmail.com>
2024-10-10 14:34:27 +0300

    101024

--- src/main/java/yeni1/Prac20.java
+++ src/main/java/yeni1/Prac20.java
@@ -2,12 +2,26 @@ package yeni1;
 
 public class Prac20 {
     public static void main(String[] args) {
-        //week 1 day week 2 day
-        for (int i = 1; i < 5; i++) {//outer loop
+        //week 1day week 2 day
+        for (int i = 1; i < 5; i++) {//outer loop row
             System.out.println("Week :" + i);
-            for (int j = 1; j <8; j++) {//inner loop
+            for (int j = 1; j <8; j++) {//inner loop column
                 System.out.println("Day :"+j);
             }
 
         }
+        //------------------------------------
+        //3 sıra 5 yıldız yazdır
+        for (int i = 1; i <4; i++) {
+            for (int j = 1; j <6; j++) {
+                System.out.print("* ");
+            }System.out.println();
+
+        }
+        //------------------------------------------
+        //
+
+
+
+
     }}
