9d1b615b9118ac6afa3a8b7b6fc94e7996a44c74
Kemal Ergorun <kemalergorun@gmail.com>
2024-10-14 14:28:17 +0300

    141024

--- src/main/java/yeni1/Prac24.java
+++ src/main/java/yeni1/Prac24.java
@@ -1,5 +1,7 @@
 package yeni1;
 
+import java.util.Arrays;
+
 public class Prac24 {
     public static void main(String[] args) {
         //int array 12,3,-3,5,23 5den küçük yazdır
@@ -12,11 +14,26 @@ public class Prac24 {
         //----------------------------------------
         //özel eleman var yok ara
         //binarysearch size o eleman index verir
-        //sort olmadan kullanma
+        //sort olmadan kullanma yasak
         //"-" negatif eleman yok demek
+        //ikiye böler öyle arar
+        System.out.println("------------------------------------");
+        int[]arr={2,1,7,6};
+        Arrays.sort(arr);
+        System.out.println(Arrays.binarySearch(arr, 2));
+        //---------------------------------------------
+        String[]str={"a","c","b","d"};
+        Arrays.sort(str);
+        System.out.println(Arrays.binarySearch(str, "b"));
+        //----------------------------------------
+        //cumle kaç kelime
+        String s="java is easy. learn java earn money.";
+        String[]words=s.split(" ");
+        System.out.println(Arrays.toString(words));
+        System.out.println(words.length);
 
 
-        
+
 
     }
 }
--- src/main/java/yeni1/Prac25.java
+++ src/main/java/yeni1/Prac25.java
@@ -1,8 +1,21 @@
 package yeni1;
 
+import java.util.Arrays;
+
 public class Prac25 {
     public static void main(String[] args) {
-        
+        //0,2,3,0,12,0
+       // sıfırları sona yazdır
+        int[]arr={0,2,3,0,12,0};
+        int[] newArr=new int[arr.length];
+        int ilkIndex=0;
+        for (int w:arr){
+            if (w!=0){
+                newArr[ilkIndex]=w;
+                ilkIndex++;
+            }
+        }
+        System.out.println(Arrays.toString(newArr));
 
     }
 }
