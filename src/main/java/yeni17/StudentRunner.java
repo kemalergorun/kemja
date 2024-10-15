3c6fcec498b0e777ba062d50405be6c2d1b11b35
Kemal Ergorun <kemalergorun@gmail.com>
2024-10-15 22:00:33 +0300

    151024

--- src/main/java/yeni17/StudentRunner.java
+++ src/main/java/yeni17/StudentRunner.java
@@ -5,6 +5,12 @@ public class StudentRunner {
         Student student=new Student();//cover with private its encapsulated
         //its very important
         //because we must hide some data
+        //getter only read setter update data
+        //getter and setter can use separately
+        //setters cant update origin only object class
+        //setters and getters non-static
+        //setters are void no return type
+
         System.out.println(student.getName());
         System.out.println(student.isSucces());
         System.out.println(student.getAge());
