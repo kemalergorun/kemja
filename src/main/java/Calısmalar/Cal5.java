71ebc8f791c039be935884d3c57040c8cbe8dd5b
Kemal Ergorun <kemalergorun@gmail.com>
2024-10-16 13:38:34 +0300

    161024

--- src/main/java/Calısmalar/Cal5.java
+++ src/main/java/Calısmalar/Cal5.java
@@ -1,8 +1,26 @@
 package Calısmalar;
 
+import java.util.ArrayList;
+
 public class Cal5 {
     public static void main(String[] args) {
-
+        //get index arraylistde index elemanı getirir
+        //set metodu verilen ögeyi yeni ile değiştirir
+        //ınt arraylist oluştur tekleri 11 yap
+        ArrayList<Integer>list1=new ArrayList<>();
+        list1.add(23);
+        list1.add(24);
+        list1.add(12);
+        list1.add(9);
+        list1.add(3);
+        System.out.println(list1);
+        //index of metodu bir arraylist içindeki bulur yoksa - verir
+        for (Integer w :list1) {
+        if(w%2!=0){
+            list1.set(list1.indexOf(w),11);
+        }
+        }
+        System.out.println(list1);
 
 
     }
