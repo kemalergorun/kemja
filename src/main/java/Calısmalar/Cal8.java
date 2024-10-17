45a226750ab74b770a510df8261f5baafb2a3c8e
Kemal Ergorun <kemalergorun@gmail.com>
2024-10-17 13:30:59 +0300

    171024

--- src/main/java/Calısmalar/Cal8.java
+++ src/main/java/Calısmalar/Cal8.java
@@ -1,7 +1,21 @@
 package Calısmalar;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Cal8 {
     public static void main(String[] args) {
+        //list oluşturmak iiçin kısa yol,
+        List<Integer>myList= Arrays.asList(1,3,4,9,7);
+        //aslist arka planda array gibi çalışır
+        //list eleman sayı değiştiren çalışmaz
+        //clear tüm elemanları siler ama kullanılamaz
+        //eleman sayısı değişmezse aslist kullanılabilir
+
+
+
+
+
 
 
     }
--- src/main/java/Calısmalar/Cal9.java
+++ src/main/java/Calısmalar/Cal9.java
@@ -2,6 +2,18 @@ package Calısmalar;
 
 public class Cal9 {
     public static void main(String[] args) {
+        //main metod içinde başka metod oluşturulamaz
+        //ancak main içinde class oluşturulursa inner class olur ve
+        //metod oluşturulur
+        //main dışı metod oluşur
+        //main sade olmalı dinamik ve hız için
+        //genelde runner classta main olur
 
     }
+    //metod oluşturma main dışı class içi oluşur
+    //access modifier+returnt ype+method name()+{}
+    //main içinden method call yapılır çalıştırılır
+
+
+
 }
