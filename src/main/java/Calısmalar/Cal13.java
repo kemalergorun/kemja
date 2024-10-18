15cf46f19e2f847ada2d7e9ae3bb91e831b7d259
Kemal Ergorun <kemalergorun@gmail.com>
2024-10-18 11:26:00 +0300

    181024

--- src/main/java/Calısmalar/Cal13.java
+++ src/main/java/Calısmalar/Cal13.java
@@ -2,6 +2,28 @@ package Calısmalar;
 
 public class Cal13 {
     public static void main(String[] args) {
+        //passbyvalue (değer ile geçiş)*******java bunla çalışır******
+        //bir değişkenin değerini veriyi metoda parametre olarak iletirsen
+        //onun kopyasını iletirsin yani origin değişmez
+        //değişken ve objelerin reference (adreslerinde)kopyalarında çalışır
+
+        //java pass by reference kullanmaz.çünkü pass by reference da
+        //original value kulladılır ve değer değişir.method origin değere
+        //ulaşır hem adres reference hem değişken origin veri drğişir
+        //ddikkat:java obje ile çalışırken heap memeoryde referans obje
+        //adrsi değişmez ama bazı obje origin içerik değişir
+        //immutable değişmez integer  string gibi wrapperlar ve enum
+        //mutable değişen arraylist hashmap stringbuilder
+        //java bunları şunun için yapar
+        //-güvenlik verilerin değişmesi engellenir
+        //-performans adres değişikliği önüne geçilerek hız sağlanır
+
+
+
+
+
+
+
 
     }
 }
