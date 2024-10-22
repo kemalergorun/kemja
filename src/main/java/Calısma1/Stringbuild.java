40a37b01bd6c9ba2180da2dcc136faf76a95d6bc
Kemal Ergorun <kemalergorun@gmail.com>
2024-10-22 12:09:12 +0300

    221024

---
+++ src/main/java/Calısma1/Stringbuild.java
@@ -0,0 +1,22 @@
+package Calısma1;
+
+public class Stringbuild {
+    public static void main(String[] args) {
+        //string builder string üretir
+        //string immutable ama builder mutable dir
+        //mutable orjinal değiştirlebilir
+        //met,n üzerinde sık değişiklik için builder kullanılır
+        //append delete insert replace metodları kullanılır
+        String s="java";
+        String t=s+"x";
+        String w=t+"?";
+        //bir string variable ile concat ile yeni obje oluşur orjin değişmez
+        //aynı değerler için string pool oluşturulur
+        //
+
+
+
+
+
+    }
+}
