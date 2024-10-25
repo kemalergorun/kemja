1c5b7ac37be19dd2bd921c9f919c25a641212126
Kemal Ergorun <kemalergorun@gmail.com>
2024-10-25 11:55:57 +0300

    251024

--- src/main/java/Calısma1/Stringb2.java
+++ src/main/java/Calısma1/Stringb2.java
@@ -17,6 +17,38 @@ public class Stringb2 {
         //belirtilen indexte karakteri siler
         sb1.replace(0,2,"hasan");
         System.out.println(sb1);
+        //--------------------------------
+        //insert belirtilen konumdan ekler
+        sb1.insert(2,"34");
+        System.out.println(sb1);
+        //compare to iki sbyi karşılaştırır
+        //int değeri ikisi eşitse 0 döner
+        //birinci önce ise - sonra ise + dönderiri
+        StringBuilder sb2=new StringBuilder("Zava");
+        StringBuilder sb3=new StringBuilder("Kava");
+        int result=sb2.compareTo(sb3);
+        System.out.println(result);
+        //-----------------------------------------
+        //tostring sb yi stringe dönüştürür
+        String str=sb1.toString().toUpperCase();
+        System.out.println(str);
+
+        //string nasıl sbye çevrilir
+        StringBuilder newSb1=new StringBuilder(str);
+        System.out.println(newSb1);
+        //-------------------------------------
+        //String buffer string ureten başka bir class
+        //mutable string olusturur
+        //string buffer multithread ama sb değil
+        //sb olmadığı için daha hızlı çalışır
+        //multi thread yapılırken işlerin sıralanması gerekir,
+        //syncronization sıraya koymaktır
+        //string buffer multi thread olduğu için syncronized dır
+        //ımmutable string class kullan
+        //mutable string buffer multi thread kullanırsa
+        //mutable string builder multi thread olmaycaksa
+
+
 
 
     }
