6766a8d09fed8f2d5a109cb530f5b9598f17bccb
Kemal Ergorun <kemalergorun@gmail.com>
2024-10-25 11:08:47 +0300

    251024

--- src/main/java/Calısma1/Stringbuild1.java
+++ src/main/java/Calısma1/Stringbuild1.java
@@ -13,7 +13,14 @@ public class Stringbuild1 {
         System.out.println(sb2.length());
         System.out.println(sb2.capacity());//aşan capacity (csayısı*2)+2 bul
         //-------------------------------------------------
-
+        //sb reverse
+        StringBuilder sb1=new StringBuilder("Java is easy");
+        System.out.println(sb1);
+        sb1.reverse();
+        System.out.println(sb1);
+        //delete charat indexte karakteri siler
+        sb1.deleteCharAt(1);
+        System.out.println(sb1);
 
 
     }
