c3acba3e46c0e2e8a9dbd289c545e601882bdd01
Kemal Ergorun <kemalergorun@gmail.com>
2024-10-28 10:25:11 +0300

    281024

---
+++ src/main/java/Calısma2/Stud1.java
@@ -0,0 +1,14 @@
+package Calısma2;
+
+public class Stud1 {
+    //static bir değişkenin veya metodun classa ait olduğunu ve
+    //classın bir objesi olmadan kullanılabileceğini belirtir
+    //static değer değişirse her obje etkilenir
+    //static variable ve metodlar class member tüm objeler için ortak
+    //static memberlar classa , non static olanlar objeye ait
+    //static class memberlere ulaşamada objeye gerek yok
+    //non static ulaşmak için obje oluşturulmalı
+    //static variable class veriable non static obje yada instance variable
+
+
+}
