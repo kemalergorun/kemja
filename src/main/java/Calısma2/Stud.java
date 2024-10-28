996339350677ba7a39742b327008eb8c27681ec8
Kemal Ergorun <kemalergorun@gmail.com>
2024-10-28 10:12:31 +0300

    281024

--- src/main/java/Calısma2/Stud.java
+++ src/main/java/Calısma2/Stud.java
@@ -9,8 +9,14 @@ public class Stud {
     //public>protected>default>private
     //public her class kullanabilir
     //protected başka package kullanamaz ancak başka package child class kullanır
+    //çocuk class kullanır
     //default  başka package kullanamaz
     //private sadece oluşan class için kullanılır
+    public String stdName="ali can";
+    protected String address="istanbul";
+    String email="alican@gmail.com";
+    private String tcKimlik="12345678";
+
 
 
 
---
+++ src/main/java/Calısma2/StudRunner.java
@@ -0,0 +1,11 @@
+package Calısma2;
+
+public class StudRunner {
+    public static void main(String[] args) {
+        //örnek instance oluşturmak
+        Stud ali=new Stud();
+        System.out.println(ali.stdName);
+        System.out.println(ali.email);
+        System.out.println(ali.address);
+    }
+}
