61ac985e044d698199c934cb9f1d941b7f379dee
Kemal Ergorun <kemalergorun@gmail.com>
2024-10-29 19:47:19 +0300

    291024

--- src/main/java/Lambda/Lambda4.java
+++ src/main/java/Lambda/Lambda4.java
@@ -1,7 +1,34 @@
 package Lambda;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Lambda4 {
     public static void main(String[] args) {
+        List<Integer>myList=new ArrayList<>(Arrays.asList(11,2,24,5,2,11,13));
+        System.out.println("myList = " + myList);
+        //methodcall
+
+        maxNumber(myList);
+        maxNumber1(myList);
 
     }
+    //create a method to find the max number from list
+    //1st way
+    public static void maxNumber(List<Integer>list){
+        System.out.println(list.stream().distinct().reduce(Integer.MIN_VALUE, (t, u) -> t > u ? t : u));
+    }
+    //2. way by using element from stream
+    public static void maxNumber1(List<Integer>list){
+        System.out.println(list.stream().distinct().reduce(list.get(0), (t, u) -> t > u ? t : u));
+    }
+
+
+
+
+
+
+
+
 }
