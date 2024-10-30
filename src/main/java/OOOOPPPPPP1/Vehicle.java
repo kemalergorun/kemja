ce97d9f9d536196ad623528ffb720593366c3a56
Kemal Ergorun <kemalergorun@gmail.com>
2024-10-30 15:19:56 +0300

    301024

---
+++ src/main/java/OOOOPPPPPP1/Vehicle.java
@@ -0,0 +1,13 @@
+package OOOOPPPPPP1;
+
+public class Vehicle {
+    //javada bir sınıf herhangi bir constructor açıkça tanımlanmamışsa
+    //java o sınıfa ait bir varsayılan constructor ekler
+    //bu parametrezsizdir ve classın kendisine aittir
+    //object ata classtır ve tüm classlar object classta metoda ulaşır
+    //tostring equals hashcode gibi
+    //++------------------------******************
+    //child class ta bir obje oluşursa constructor parenttan alta doğru
+    //çalışır*********önemli****************
+
+}
