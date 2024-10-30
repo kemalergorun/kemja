6c0fd1a1860f72fbe1bd8df7abac56cdf2f624b2
Kemal Ergorun <kemalergorun@gmail.com>
2024-10-30 11:51:55 +0300

    301024

---
+++ src/main/java/Calısma3/Instance1.java
@@ -0,0 +1,33 @@
+package Calısma3;
+
+public class Instance1 {
+    //instance blok
+    {
+        System.out.println("merhaba java");
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+     //*instance örnek blok bir class ın her objeyi oluşturduğunda çalışır
+        //1 her örnek için çalışır yani her oluşan obje için çalışır
+        //2 kod tekrarını önler instance kod blokları kullanılarak
+        //tekrar tekrar kod yazmadan kurtulunur
+        //bir araba üretilirken ortak nokta bulunur ve yazılır
+        //her araba için kullanılır
+
+
+        Instance1 obj1=new Instance1();
+
+
+
+
+
+
+
+
+
+    }
+}
