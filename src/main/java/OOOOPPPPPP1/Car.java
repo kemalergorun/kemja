9f672082f06b3304930e84e4f7a247516bc2dfe0
kemalergorun <kemalergorun@gmail.com>
2024-11-19 11:57:48 +0300

    191124

--- src/main/java/OOOOPPPPPP1/Car.java
+++ src/main/java/OOOOPPPPPP1/Car.java
@@ -8,7 +8,16 @@ public class Car extends Vehicle{
     public Car(String tip){
         super("honda",2024,"elektrikli");
         System.out.println("sport");
-//parametreliler paramları çağırır paramatresizler parametresizleri çağırır
+
+         //parametreliler paramları çağırır
+        // paramatresizler parametresizleri çağırır
+
+        // aynı class ta consturoctorlar arasında gezinebilir yani classtan
+        //hem sedan hem de sport üretime sokulur
+        //aynı class ta cons arasında gezinmek için super sil this parametresiz
+        //seçer this ve super ilk satırda olmalıdır bu yüzden ikisi bir arada
+        //aynı consta kullanılamazlar
+
 
     }
 }
