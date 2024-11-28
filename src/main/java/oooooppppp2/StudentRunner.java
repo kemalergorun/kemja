d6181edd5c80134c2ac0441425386ca12519b2a9
kemalergorun <kemalergorun@gmail.com>
2024-11-28 22:14:55 +0300

    281124

--- src/main/java/oooooppppp2/StudentRunner.java
+++ src/main/java/oooooppppp2/StudentRunner.java
@@ -6,6 +6,29 @@ public class StudentRunner {
         System.out.println(s.getAge());
         System.out.println(s.isSuccessful());
 
+        //-------set-----///
+        s.setAge(25);
+        System.out.println(s.getAge());
+
+        s.setSuccessful(false);
+        System.out.println(s.isSuccessful());
+        //class ta obje değerleri değişmez
+        //obje değerleri değişir
+        //obje class ın kopyasıdır
+        //orjinal kalıplar hiçbir zaman değişmez
+        //set ile veriler değişir constructor ile değişir
+        //set mevcut nesnenin verilerini değişitirir
+        //constructor yeni obje başlangıç durumunu belirler
+        //get ve set metodlarının olduğu classlar java bean olarak anılır
+        //-----*****************----onemli----***********----
+        //encaps bir obje oluştur iki obje gibi kullan
+        //kalıptan istediğimiz kadar oluştururuz
+        //memory problemi önlenir birçok obje memori doldurur
+
+
+
+
+
 
     }
 }
